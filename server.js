53929f7a8a29f127b37cd3fa83a9d067106d4ba2
amaannii <amaaanniii@gmail.com>
2026-01-05 15:09:49 +0530

    added

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import express from "express"
+import connected from "./database/connect.js";
+import dotenv, { config } from "dotenv"
+
+dotenv.config()
 
 const app=express()
+
+connected
+
 
 app.listen(3000,()=>{
 console.log("server is connected succefully");
